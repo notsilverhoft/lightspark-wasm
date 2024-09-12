f6367b808dc0d1129acfc7b24dbeede03a6db45c
jraaff27 <jraaff27@weldre4.org>
2024-09-12 02:03:35 +0000

    Header Patches 2

--- Fixed-Header-Files/fix.sh
+++ Fixed-Header-Files/fix.sh
@@ -6,3 +6,8 @@ fix_dir=$(pwd)
     sudo cp $fix_dir/gtypes.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/gtypes.h
     sudo cp $fix_dir/glibconfig.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/lib/glib-2.0/include/glibconfig.h
     sudo cp $fix_dir/glib.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib.h
+    sudo cp $fix_dir/garray.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/garray.h
+    sudo cp $fix_dir/gasyncqueue.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/gasyncqueue.h
+    sudo cp $fix_dir/gthread.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/gthread.h
+    sudo cp $fix_dir/gatomic.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/gatomic.h
+    sudo cp $fix_dir/glib-typeof.h $working_dir/PKGCONFIG/pango-cairo-wasm/build/include/glib-2.0/glib/glib-typeof.h
